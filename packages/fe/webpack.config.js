6d717239cd142348a32c3747be0a0f7a07c5f3d0
codeliner <kontakt@codeliner.ws>
2023-05-04 23:43:31 +0200

    Add backend app

--- packages/fe/webpack.config.js
+++ packages/fe/webpack.config.js
@@ -5,5 +5,9 @@ const { withReact } = require('@nx/react');
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.devServer = {
+    ...config.devServer,
+    port: 4200
+  };
   return config;
 });
