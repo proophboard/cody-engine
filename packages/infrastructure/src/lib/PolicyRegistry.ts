fc9706eb8cbede40085e9c1e13d02fb0ae492499
codeliner <kontakt@codeliner.ws>
2023-07-03 21:58:41 +0200

    fix: Circular deps II

---
+++ packages/infrastructure/src/lib/PolicyRegistry.ts
@@ -0,0 +1,5 @@
+import {Event} from "@event-engine/messaging/event";
+import {PolicyDescription} from "@event-engine/descriptions/descriptions";
+
+export type Policy = (event: Event, deps: any) => Promise<void>;
+export type PolicyRegistry = {[eventName: string]: {[policyName: string]: { policy: Policy, desc: PolicyDescription }}};
