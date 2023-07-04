fa0d29276118bac136458dd24d1733f93a8fec28
codeliner <kontakt@codeliner.ws>
2023-07-04 12:48:29 +0200

    fix: WithButtonProps is missing in contribution template

--- packages/contribution/src/generators/prepare-contribution/files/fe/src/app/components/commands.ts
+++ packages/contribution/src/generators/prepare-contribution/files/fe/src/app/components/commands.ts
@@ -1,7 +1,8 @@
 import React from "react";
 import FleetManagementAddCarToFleet  from "@frontend/app/components/fleet-management/commands/AddCarToFleet";
+import {WithCommandButtonProps} from "@frontend/app/components/core/CommandButton";
 
-export type CommandComponentRegistry = {[commandName: string]: React.FunctionComponent};
+export type CommandComponentRegistry = {[commandName: string]: React.FunctionComponent<any & WithCommandButtonProps>};
 
 export const commands: CommandComponentRegistry = {
   "FleetManagement.AddCarToFleet": FleetManagementAddCarToFleet
