c9ff006a8932a1983c7b0d458e7018a45c9cf2ad
codeliner <kontakt@codeliner.ws>
2023-07-12 21:20:54 +0200

    feat(FE) Use app name as doc title
    Also use prooph board favicon instead of nx

--- packages/fe/src/main.tsx
+++ packages/fe/src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
 import App from './app/app';
+import {environment} from "@frontend/environments/environment";
+
+document.title = environment.appName;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-      <App />
+    <App />
   </StrictMode>
 );
