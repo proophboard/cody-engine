373ba11f9a2445b44a6f4dd1fe66fdeae3695f4a
bfalke <b.falke@bitexpert.de>
2023-07-26 10:43:41 +0200

    Rename 'viewName' to 'valueObjectName' for consistency

--- packages/fe/src/util/components/load-view-components.ts
+++ packages/fe/src/util/components/load-view-components.ts
@@ -1,16 +1,16 @@
 import React from "react";
 import {views as viewComponents} from "@frontend/app/components/views";
 import {views as overwriteViewComponents} from "@frontend/extensions/app/components/views";
-export const loadViewComponent = (viewName: string): React.FunctionComponent<any> => {
-  if(overwriteViewComponents[viewName]) {
+export const loadViewComponent = (valueObjectName: string): React.FunctionComponent<any> => {
+  if(overwriteViewComponents[valueObjectName]) {
 
-    return overwriteViewComponents[viewName];
+    return overwriteViewComponents[valueObjectName];
   }
 
-  if(viewComponents[viewName]) {
+  if(viewComponents[valueObjectName]) {
 
-    return viewComponents[viewName];
+    return viewComponents[valueObjectName];
   }
 
-  throw new Error(`No view component registered for value object: "${viewName}". Please check the registry file: packages/fe/src/app/components/views.ts!`);
+  throw new Error(`No view component registered for value object: "${valueObjectName}". Please check the registry file: packages/fe/src/app/components/views.ts!`);
 }
