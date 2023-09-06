c3b6aa11ca057b03e811633d766718697b825302
Kai Hillenbrand <kai@torchedhill.com>
2023-09-06 15:52:51 +0200

    some more WIP stuff for finding the relevant nodes to be tested in the right order; needs to be reworked and cleaned up

--- packages/cody/src/lib/hooks/on-feature.ts
+++ packages/cody/src/lib/hooks/on-feature.ts
@@ -1,4 +1,4 @@
-import { CodyHook, Node } from "@proophboard/cody-types";
+import { CodyHook, Node, NodeType } from "@proophboard/cody-types";
 import { parseJsonMetadata } from "@proophboard/cody-utils";
 import { Context } from "./context";
 import { CodyResponseException } from "./utils/error-handling";
@@ -15,18 +15,63 @@ export const onFeature: CodyHook<Context> = async (feature: Node, ctx: Context) 
     const parentContainer = feature.getParent();
     const parentContainerMeta : any = parentContainer?.getMetadata() ? parseJsonMetadata<{service?: string}>(parentContainer) : {};
     // console.log(parentContainerMeta);
-    
-    // feature.getChildren().forEach(function(elem) {
-    //   console.log(elem.getName(), elem.getSources().first()?.getName(), elem.getTargets().first()?.getName());
-    // });
 
     // check if either the feature (the test) or its bounded context (the test container) have their mode set to "test"
     if (featureMeta[modeKey] == modeValueTest || parentContainerMeta[modeKey] == modeValueTest) {
 
-      // TODO: generate & run tests
+      let whenCommand : Node | undefined;
+    
+      // find "when" command node
+      feature.getChildren().forEach(function(elem) {
+        if (elem.getType() == NodeType.command) {
+          whenCommand = elem;
+          console.log('found WHEN command:', elem.getName(), elem.getSources().first()?.getName(), elem.getTargets().first()?.getName());
+        }
+      });
 
-      return {
-        cody: `Running test called "${feature.getName().trim()}".`,
+      if (whenCommand) {
+        let givenNodes : Array<Node> = [];
+        let thenNodes : Array<Node> = [];
+        let currentNode : Node = whenCommand;
+
+        console.log("checking for GIVEN");
+
+        // everything before the "when" command node is seen as "given"
+        while (!currentNode.getSources().isEmpty()) {
+          currentNode = currentNode.getSources().first();
+          givenNodes.unshift(currentNode);
+
+          console.log(currentNode.getName(), currentNode.getSources().count());
+        }
+
+        // TODO: the source/target lists aren't recursive, so they always only give you direct source/targets, and not their sources/targets! we'd have to fetch those nodes again, somehow
+
+        console.log("checking for THEN");
+
+        // everything after the "when" command is "then"
+        currentNode = whenCommand;
+        while (!currentNode.getTargets().isEmpty()) {
+          currentNode = currentNode.getTargets().first();
+          thenNodes.push(currentNode);
+
+          console.log(currentNode.getName());
+        }
+        
+        // for logging:
+        var loggedNodes : Array<String> = [];
+        givenNodes.forEach(function(node) {
+          loggedNodes.push(node.getName());
+        });
+        loggedNodes.push(whenCommand.getName());
+        thenNodes.forEach(function(node) {
+          loggedNodes.push(node.getName());
+        });
+
+        console.log("running test!");
+
+        return {
+          cody: `Running test called "${feature.getName().trim()}".\nThese are the nodes included in the test: ${loggedNodes.toString()}`,
+        }
       }
     }
 
