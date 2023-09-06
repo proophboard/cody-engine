cc904ed146e3aef8a1e9fbadc521ca6d3667337d
bfalke <b.falke@bitexpert.de>
2023-09-06 16:42:15 +0200

    WIP

--- packages/be/cucumber.js
+++ packages/be/cucumber.js
@@ -2,6 +2,7 @@
 let common = [
   'features/**/*.feature',                // Specify our feature files
   '--require-module ts-node/register', //typescript cucumber
+  '--require-module tsconfig-paths/register', //typescript cucumber
   '--require ./features/step_definitions/**/*.ts',   // Load step definitions
 ].join(' ');
 
