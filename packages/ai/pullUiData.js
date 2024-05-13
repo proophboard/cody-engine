ba3601cf6c92cfd281dd8831905846a1a916d3f9
Himayat Kamran <himayatkamran@hotmail.de>
2024-05-13 18:04:12 +0200

    Remove unused AI-related files and update npm scripts

--- packages/ai/pullUiData.js
+++
@@ -1,8 +0,0 @@
-// Hier werden die Aktuellen UI-Daten geholt die Himayat gefunden hat.
-
-
-//return: Die daten die aus dem vorhandenen Frontend gezogen werden sollen
-export function pullUIData(){
-
-    return null;
-}
--- packages/ai/saveUiData.js
+++
@@ -1,6 +0,0 @@
-//Hier wird die Persistenz der Daten gemanaget.
-
-//Data: Die daten die im gespeichert werden sollen.
-export function saveUIData(Data){
-
-}
