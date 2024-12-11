2daa491bd1390a2c07bd6abad4dc968b9233a6c2
codeliner <kontakt@codeliner.ws>
2024-12-11 18:40:53 +0100

    Provide a sort option for filtered enum

--- packages/fe/src/app/components/core/form/widgets/FilteredEnumWidget.tsx
+++ packages/fe/src/app/components/core/form/widgets/FilteredEnumWidget.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {WidgetProps} from "@rjsf/utils";
+import {EnumOptionsType, WidgetProps} from "@rjsf/utils";
 import {useUser} from "@frontend/hooks/use-user";
 import {usePageData} from "@frontend/hooks/use-page-data";
 import {useParams} from "react-router-dom";
@@ -22,14 +22,23 @@ const FilteredEnumWidget = (props: FilteredEnumWidgetProps) => {
   const enumVals = props.options.enumOptions || [];
   const filter = props.options.filter as string || 'true';
   const map = props.options.map as string || 'item';
+  const sort = props.options.sort === 'desc' ? -1 : 1;
   const selectedVal = props.value;
+
+  const optionalSort = (arrayToSort: EnumOptionsType[]): EnumOptionsType[] => {
+    if(!props.options.sort) {
+      return arrayToSort;
+    }
+
+    return arrayToSort.sort((a,b) => a.label.localeCompare(b.label) * sort);
+  }
 
   const filteredProps = {
     ...props, options: {
       ...props.options,
-      enumOptions: enumVals
+      enumOptions: optionalSort(enumVals
         .filter(item => item.value === selectedVal || jexl.evalSync(filter, {...jexlCtx, item: item}))
-        .map(item => jexl.evalSync(map, {...jexlCtx, item: item}))
+        .map(item => jexl.evalSync(map, {...jexlCtx, item: item})))
     }
   };
 
