1a3004a6df6f1e80e63bbafdde11c49929706a97
Marvin Taeffner <m.taeffner@bitexpert.de>
2025-05-13 14:12:23 +0200

    [#88] update styling of component PlayDroppable

--- packages/play/src/app/components/core/PlayDroppable.tsx
+++ packages/play/src/app/components/core/PlayDroppable.tsx
@@ -10,14 +10,15 @@ const PlayDroppable = ({ id }: TPlayDroppable) => {
   const { isOver, setNodeRef } = useDroppable({
     id,
   });
-  // TODO update styling
   const sx = {
+    width: '100%',
     height: '2.25rem',
-    borderWidth: '3px',
+    borderWidth: '2px',
     borderStyle: 'dotted',
     borderColor: theme.palette.grey['500'],
+    borderRadius: '0.25rem',
     backgroundColor: isOver
-      ? theme.palette.grey['400']
+      ? theme.palette.grey['300']
       : theme.palette.grey['200'],
   };
 
