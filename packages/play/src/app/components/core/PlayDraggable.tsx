6d5ebea1fb22ed64220ab336fd788e458f6ea7f8
Marvin Taeffner <m.taeffner@bitexpert.de>
2025-05-13 14:43:34 +0200

    [#88] update styling of component PlayDraggable

--- packages/play/src/app/components/core/PlayDraggable.tsx
+++ packages/play/src/app/components/core/PlayDraggable.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { useDraggable } from '@dnd-kit/core';
-import { Box } from '@mui/material';
+import { Box, useTheme } from '@mui/material';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 
 type TPlayDraggable = {
   id: string;
@@ -9,15 +10,35 @@ type TPlayDraggable = {
 };
 
 const PlayDraggable = ({ id, isDragDropEnabled, children }: TPlayDraggable) => {
-  const { attributes, listeners, setNodeRef } = useDraggable({ id });
+  const theme = useTheme();
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({ id });
+  const sx = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '0.25rem',
+    borderRadius: '0.25rem',
+    backgroundColor: theme.palette.grey['300'],
+    padding: '0.25rem',
+    cursor: 'move',
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : 'none',
+    zIndex: transform ? theme.zIndex.drawer + 1 : 'auto',
+    '.children-wrapper button': {
+      pointerEvents: 'none',
+    },
+  };
 
   return (
     <>
       {!isDragDropEnabled ? (
         children
       ) : (
-        <Box ref={setNodeRef} {...listeners} {...attributes}>
-          {children}
+        <Box ref={setNodeRef} {...listeners} {...attributes} sx={sx}>
+          <Box flex={1} className="children-wrapper">
+            {children}
+          </Box>
+          <DragIndicatorIcon />
         </Box>
       )}
     </>
