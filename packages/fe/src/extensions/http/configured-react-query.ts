33643a038289873b834942230899b7b5a11f35c8
codeliner <kontakt@codeliner.ws>
2025-05-15 15:44:01 +0200

    fix: Disable queryClient to refetch on window focus

--- packages/fe/src/extensions/http/configured-react-query.ts
+++ packages/fe/src/extensions/http/configured-react-query.ts
@@ -3,7 +3,14 @@ import {QueryClient} from "@tanstack/react-query";
 let queryClient: QueryClient | undefined = undefined;
 
 if(typeof queryClient === "undefined") {
-  queryClient = new QueryClient({});
+  queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // Attention: if you enable this option, command dialogs close on window focus
+        refetchOnWindowFocus: false
+      }
+    }
+  });
 }
 
 export default queryClient;
