cad72dc765af719f953e4a97562e7fa4573db172
Marvin Taeffner <m.taeffner@bitexpert.de>
2025-05-15 14:46:09 +0200

    [#88] add util function updatePageButtonPosition

---
+++ packages/play/src/app/utils/updatePageButtonPosition.ts
@@ -0,0 +1,57 @@
+import {
+  Action,
+  CodyPlayConfig,
+  getEditedContextFromConfig,
+} from '@cody-play/state/config-store';
+import { playDefinitionIdFromFQCN } from '@cody-play/infrastructure/cody/schema/play-definition-id';
+import { ActionContainerInfo } from '@frontend/app/components/core/form/types/action';
+
+const updatePageButtonPosition = (
+  config: CodyPlayConfig,
+  dispatch: (a: Action) => void,
+  containerInfo: ActionContainerInfo,
+  buttonPosition: string,
+  additionalData?: Record<string, any>
+) => {
+  const ctx = getEditedContextFromConfig(config);
+  const informationRuntimeInfo = config.types[containerInfo.name];
+  const uiOptions = informationRuntimeInfo.uiSchema?.['ui:options'];
+  const uiOptionsActions = uiOptions
+    ? (uiOptions.actions as object[])
+    : undefined;
+  const actions = uiOptionsActions
+    ? uiOptionsActions.map((action) => ({
+        ...action,
+        position:
+          // @ts-expect-error TS2339: Property command does not exist on type object
+          additionalData && additionalData.command === action.command
+            ? buttonPosition
+            : // @ts-expect-error TS2339: Property position does not exist on type object
+              action.position,
+      }))
+    : [];
+  const information = {
+    ...informationRuntimeInfo,
+    uiSchema: {
+      ...informationRuntimeInfo.uiSchema,
+      'ui:options': {
+        ...uiOptions,
+        actions,
+      },
+    },
+  };
+  const definitionId = playDefinitionIdFromFQCN(containerInfo.name);
+
+  dispatch({
+    ctx,
+    type: 'ADD_TYPE',
+    name: containerInfo.name,
+    information,
+    definition: {
+      definitionId,
+      schema: config.definitions[definitionId],
+    },
+  });
+};
+
+export default updatePageButtonPosition;
