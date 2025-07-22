bffc69b21b04715e3420d217c82e8720cb0a693e
codeliner <kontakt@codeliner.ws>
2025-07-22 22:36:07 +0200

    Suggest disable checks based on enum and boolean props

--- packages/play/src/infrastructure/vibe-cody/button-instructions/disable-button.tsx
+++ packages/play/src/infrastructure/vibe-cody/button-instructions/disable-button.tsx
@@ -4,6 +4,14 @@ import {FocusedButton} from "@cody-play/state/focused-element";
 import {playIsCodyError} from "@cody-play/infrastructure/cody/error-handling/with-error-check";
 import {getLabelFromInstruction} from "@cody-play/infrastructure/vibe-cody/utils/text/get-label-from-instruction";
 import { Cancel } from "mdi-material-ui";
+import {PlayInformationRuntimeInfo} from "@cody-play/state/types";
+import {playNodeLabel, playServiceFromFQCN} from "@cody-play/infrastructure/cody/schema/play-definition-id";
+import {registryIdToDataReference} from "@app/shared/utils/registry-id-to-data-reference";
+import {getAllTypesOnPage} from "@cody-play/infrastructure/vibe-cody/utils/types/get-all-types-on-page";
+import {isQueryableStateDescription, isStateDescription} from "@event-engine/descriptions/descriptions";
+import {Schema} from "@cody-play/infrastructure/vibe-cody/utils/schema/schema";
+import {JSONSchema7} from "json-schema";
+import {get} from "lodash";
 
 const makeDisableButtonViaExpr = (): Instruction => {
   const TEXT = `Disable the button via expression`
@@ -37,11 +45,110 @@ const makeDisableButtonViaExpr = (): Instruction => {
   }
 }
 
+const makeDisableButtonWhenStatePropIsValue = (stateVO: PlayInformationRuntimeInfo, prop: string, val: unknown): Instruction => {
+  const stateLabel = playNodeLabel(stateVO.desc.name);
+
+  const TEXT = `Disable the button when ${stateLabel} ${prop} is "${val}"`;
+
+  return {
+    text: TEXT,
+    icon: <Cancel />,
+    noInputNeeded: true,
+    isActive: () => true,
+    match: input => input.startsWith(TEXT),
+    execute: async (input, ctx, dispatch, config) => {
+      const success = await setButtonProperty(
+        ctx.focusedElement! as FocusedButton,
+        'disabled:expr',
+        `$> page|data('${registryIdToDataReference(stateVO.desc.name)}')|get('${prop}') == ${typeof val === 'string' ? `'${val}'` : val}`,
+        config,
+        dispatch
+      )
+
+      if (playIsCodyError(success)) {
+        return success;
+      }
+
+      return {
+        cody: `Button is now disabled when ${stateLabel} ${prop} is "${val}".`
+      }
+    }
+  }
+}
+
+const makeDisableButtonWhenStatePropIsNotValue = (stateVO: PlayInformationRuntimeInfo, prop: string, val: unknown): Instruction => {
+  const stateLabel = playNodeLabel(stateVO.desc.name);
+
+  const TEXT = `Disable the button when ${stateLabel} ${prop} is not "${val}"`;
+
+  return {
+    text: TEXT,
+    icon: <Cancel />,
+    noInputNeeded: true,
+    isActive: () => true,
+    match: input => input.startsWith(TEXT),
+    execute: async (input, ctx, dispatch, config) => {
+      const success = await setButtonProperty(
+        ctx.focusedElement! as FocusedButton,
+        'disabled:expr',
+        `$> page|data('${registryIdToDataReference(stateVO.desc.name)}')|get('${prop}') != ${typeof val === 'string' ? `'${val}'` : val}`,
+        config,
+        dispatch
+      )
+
+      if (playIsCodyError(success)) {
+        return success;
+      }
+
+      return {
+        cody: `Button is now disabled when ${stateLabel} ${prop} is not "${val}".`
+      }
+    }
+  }
+}
+
 export const DisableButtonProvider: InstructionProvider = {
   isActive: (context, config) => !!context.focusedElement && context.focusedElement.type === "button",
   provide: (context, config) => {
-    return [
+
+    const pageConfig = context.page.handle.page;
+
+    const types = getAllTypesOnPage(pageConfig, config);
+
+    const instructions: Instruction[] = [
       makeDisableButtonViaExpr(),
-    ]
+    ];
+
+    types.forEach(type => {
+      if(isQueryableStateDescription(type.desc)) {
+        let stateSchema = new Schema(type.schema as JSONSchema7, true);
+
+        if(stateSchema.isRef()) {
+          stateSchema.resolveRef(playServiceFromFQCN(type.desc.name), config.types);
+        }
+
+        const stateJSONSchema = stateSchema.toJsonSchema();
+
+        const properties = get(stateJSONSchema, 'properties', {} as Record<string, any>);
+
+        for (const prop in properties) {
+          const propSchema: JSONSchema7 = properties[prop] as JSONSchema7;
+
+          if(propSchema.type && propSchema.type === "boolean") {
+            instructions.push(makeDisableButtonWhenStatePropIsValue(type, prop, true))
+            instructions.push(makeDisableButtonWhenStatePropIsValue(type, prop, false))
+          }
+
+          if(propSchema.enum) {
+            propSchema.enum.forEach(val => {
+              instructions.push(makeDisableButtonWhenStatePropIsValue(type, prop, val));
+              instructions.push(makeDisableButtonWhenStatePropIsNotValue(type, prop, val));
+            })
+          }
+        }
+      }
+    })
+
+    return instructions;
   }
 }
