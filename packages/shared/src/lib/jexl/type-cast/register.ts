19961e4ce8a0c2419f136e7b9bec3b5f13c64d6d
codeliner <kontakt@codeliner.ws>
2025-08-19 16:07:58 +0200

    Add a toLocal type cast for numbers and date objects

--- packages/shared/src/lib/jexl/type-cast/register.ts
+++ packages/shared/src/lib/jexl/type-cast/register.ts
@@ -4,8 +4,22 @@ export const registerTypeCastExtensions = (jexl: Jexl): void => {
   jexl.addTransform('toInt', (val: string) => parseInt(val));
   jexl.addTransform('toFloat', (val: string) => parseFloat(val));
   // toString() seems to be reserved in Jexl, so we have to use toStr
-  jexl.addTransform('toStr', (val: any) => val instanceof Error ? val.message : typeof val === "object" ? JSON.stringify(val) : '' + val);
+  jexl.addTransform('toStr', toStr);
   jexl.addTransform('toJSON', (val: any, space?: number) => JSON.stringify(val, null, space));
   jexl.addTransform('fromJSON', (val: string): any => JSON.parse(val));
   jexl.addTransform('toArray', (val: any) => Array.isArray(val) ? val : [val]);
+  jexl.addTransform('toLocal', toLocaleString);
+}
+
+const toStr = (val: any) => val instanceof Error ? val.message : typeof val === "object" ? JSON.stringify(val) : '' + val;
+const toLocaleString = (val: any) => {
+  if(typeof val === "undefined") {
+    return '';
+  }
+
+  if(typeof val.toLocaleString === "function") {
+    return val.toLocaleString();
+  }
+
+  return toStr(val);
 }
